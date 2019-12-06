d09f9c166679c5da350277c0b1b358a14e117aa2
Aaron <ahelman@csumb.edu>
2019-12-05 20:15:38 -0800

    image upload??

--- src/main/java/edu/csumb/cst438/EnFrame/controllers/PhotoController.java
+++ src/main/java/edu/csumb/cst438/EnFrame/controllers/PhotoController.java
@@ -1,12 +1,15 @@
 package edu.csumb.cst438.EnFrame.controllers;
 
 import edu.csumb.cst438.EnFrame.services.PhotoService;
+import java.io.File;
+import java.io.IOException;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import edu.csumb.cst438.EnFrame.models.Photo;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 @SpringBootApplication
 @RestController
@@ -14,12 +17,15 @@ import org.springframework.web.bind.annotation.*;
 public class PhotoController {
     @Autowired
     PhotoService photoService;
-    
+
     @RequestMapping(method = RequestMethod.POST, value = "/photo/uploadPhoto")
     @ResponseBody
-    public Boolean uploadPhoto(@RequestBody Photo photo)
-    {
-        return photoService.uploadPhoto(photo);
+    public Boolean uploadPhoto(@RequestPart(value = "file") MultipartFile image) throws IOException {
+        if(image == null){
+            return false;
+        }
+            return photoService.uploadPhoto(image);
+
     }
 
     @RequestMapping(method = RequestMethod.GET, value = "/photo/getPhotosByTag")
--- src/main/java/edu/csumb/cst438/EnFrame/services/PhotoService.java
+++ src/main/java/edu/csumb/cst438/EnFrame/services/PhotoService.java
@@ -5,12 +5,21 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
 import edu.csumb.cst438.EnFrame.models.Photo;
 import edu.csumb.cst438.EnFrame.repository.PhotoRepository;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
 import org.springframework.stereotype.Service;
+
+import com.amazonaws.AmazonServiceException;
+import com.amazonaws.SdkClientException;
 import com.amazonaws.auth.AWSCredentials;
 import com.amazonaws.auth.AWSStaticCredentialsProvider;
 import com.amazonaws.auth.BasicAWSCredentials;
@@ -24,26 +33,37 @@ public class PhotoService {
     PhotoRepository photoRepo;
     Photo photo;
 
-    /*
-    AWSCredentials credentials = new BasicAWSCredentials("AKIAJZXSN226UE22E4IA",
-                "SJhX0wud1FpY54e4KrX3wMsNrIcqAwDC3cypLGyn");
-    AmazonS3 s3client = AmazonS3ClientBuilder.standard()
-                .withCredentials(new AWSStaticCredentialsProvider(credentials)).withRegion(Regions.US_WEST_1).build();
-    public void uploadImage(String objectKey, File image){
+    
+    
+    // public void uploadImage(String objectKey, File image){
         
-    }
-    public void deleteImage(String objectKey){
-        s3client.deleteObject("test-bucket438", objectKey);
-    }
-    */
-    public Boolean uploadPhoto(Photo photo) {
-
+    // }
+    // public void deleteImage(String objectKey){
+    //     s3client.deleteObject("test-bucket438", objectKey);
+    // }
+    // */
+    public Boolean uploadPhoto(MultipartFile image) throws SdkClientException, AmazonServiceException {
         //Do the stuff to upload photo on Amazon Bucket
         //objectKey is what we will name the file
         //File image is the image 
-        //s3client.putObject("test-bucket438", objectKey, image);
+        // s3client.putObject("test-bucket438", photo.getReference(), image.getInputStream(), image.;
+        AWSCredentials credentials = new BasicAWSCredentials("AKIAJZXSN226UE22E4IA",
+                "SJhX0wud1FpY54e4KrX3wMsNrIcqAwDC3cypLGyn");
+        AmazonS3 s3client = AmazonS3ClientBuilder.standard()
+                .withCredentials(new AWSStaticCredentialsProvider(credentials)).withRegion(Regions.US_WEST_1).build();
+        try {
+            File convFile = new File(image.getOriginalFilename());
+            convFile.createNewFile();
+            FileOutputStream fos = new FileOutputStream(convFile);
+            fos.write(image.getBytes());
+            fos.close();
+            s3client.putObject("test-bucket438", "pizza.jpg", convFile);
+        } catch (IOException e){
+            
+        }
 
-        return photoRepo.insertIfExist(photo);
+        // return photoRepo.insertIfExist(photo);
+        return true;
     }
 
     public Iterable<Photo> getPhotosByTag(String tag) {
