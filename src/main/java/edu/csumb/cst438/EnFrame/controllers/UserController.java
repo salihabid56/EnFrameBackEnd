de2084b322a52e4caed60f0dcff27c403a9dd491
Eric Orozco Viscarra <eorozco-viscarra@csumb.edu>
2019-12-09 19:13:01 -0800

    Revert "getUser endpoint added"

--- src/main/java/edu/csumb/cst438/EnFrame/controllers/UserController.java
+++ src/main/java/edu/csumb/cst438/EnFrame/controllers/UserController.java
@@ -22,13 +22,6 @@ public class UserController {
         return userService.addUser(user);
     }
 
-    @RequestMapping(method = RequestMethod.GET, value = "/user/getUser")
-    @ResponseBody
-    public User getUser(@RequestParam String email)
-    {
-        return userService.getUser(email);
-    }
-
     @RequestMapping(method = RequestMethod.DELETE, value = "/user/deleteUser")
     @ResponseBody
     public Boolean deleteUser(@RequestParam String email)
